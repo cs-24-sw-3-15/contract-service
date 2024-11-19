f64daa359193afe896f31a20f19843b640780c47
Kresten Laust <krestenlaust@gmail.com>
2024-11-19 12:06:29 +0100

    Add test

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class DocumentTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "does not accept files larger than 50 MiB" do
+    document = Document.new(title: "Test document")
+    file = mock_file(size: 50.megabytes, content_type: "application/pdf", filename: "testfile.pdf")
+    document.file.attach(io: file)
+
+    assert_not document.valid?
+  end
+
+  # Helper method to create a mock file
+  def mock_file(size:, content_type:, filename:)
+    file_mock = StringIO.new
+    def file_mock.size
+      @size
+    end
+    file_mock.instance_variable_set(:@size, size)
+    file_mock.define_singleton_method(:original_filename) { filename }
+    file_mock.define_singleton_method(:content_type) { content_type }
+    file_mock
   end
 end
