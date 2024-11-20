8b763199dacfa662e89982268e367b2aa0386dcd
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-11-20 15:08:36 +0100

    added more menu options and icons

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :contracts, only: [ :index, :show, :new, :create, :destroy ]
   get "dashboard" => "dashboard#index", as: :dashboard
+  get "suppliers" => redirect("/contracts"), status: 302
+  get "affiliates" => redirect("/contracts"), status: 302
+  get "users" => redirect("/contracts"), status: 302
+  get "pending_contracts" => redirect("/contracts"), status: 302
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
