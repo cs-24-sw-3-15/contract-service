c28f70a1636826c19583bb8176eb90f64ebdbf7f
casstef <casper_steffensen@yahoo.co.uk>
2024-11-26 12:14:51 +0100

    feat: refine class attributes (#38)
    * feat: refine class attributes
    
    * Refactor contract attributes
    
    * Lint & fix test

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_21_222515) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.integer "created_by_id", null: false
     t.index ["created_by_id"], name: "index_contracts_on_created_by_id"
   end
