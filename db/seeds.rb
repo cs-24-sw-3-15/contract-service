77de27e1576c9a4323bf11ae14eb40d74dc61a27
Sofie <sofie+git@mailbox.org>
2024-11-27 14:09:10 +0000

    feat: add db:seeds following fixutre data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+joe = User.where(name: "Joe", email: "joe@example.com").first_or_create!(
+  role: "user",
+  password: "example",
+)
+
+jane = User.where(name: "Jane", email: "jane@example.com").first_or_create!(
+  role: "admin",
+  password: "example",
+)
+
+Contract.where(title: "Basic Contract").first_or_create!(
+  start_date: Date.today + 1.day,
+  end_date: Date.today + 31.days,
+  created_by: joe,
+  documents_attributes: [
+    {
+      title: "Basic PDF File",
+      created_by: joe,
+      file: File.open(Rails.root.join("test/fixtures/files/basic_document.pdf"))
+    }
+  ]
+)
+
+Contract.where(title: "Advanced Contract").first_or_create!(
+  start_date: Date.today,
+  end_date: Date.today + 365.days,
+  created_by: jane,
+  documents_attributes: [
+    {
+      title: "My Advanced PDF File",
+      created_by: jane,
+      file: File.open(Rails.root.join("test/fixtures/files/advanced_document_1.pdf"))
+    },
+    {
+      title: "My Suppliementary PDF File",
+      created_by: jane,
+      file: File.open(Rails.root.join("test/fixtures/files/advanced_document_2.pdf"))
+    }
+  ]
+)
