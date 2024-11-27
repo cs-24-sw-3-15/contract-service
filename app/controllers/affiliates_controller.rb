f9f89e9f9db9e106f161a1e0abed161048a35d59
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-11-27 22:13:15 +0100

    resolved comments

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -1,17 +1,16 @@
 class AffiliatesController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_affiliate, except: :index
 
   def index
     @affiliates = policy_scope(Affiliate)
   end
 
   def new
-    @affiliate = Affiliate.new
+    @affiliate = authorize Affiliate.new
   end
 
   def create
-    @affiliate = Affiliate.new(affiliate_params)
+    @affiliate = authorize Affiliate.new(affiliate_params)
     if @affiliate.save
       redirect_to affiliates_path, notice: 'Affiliate was successfully created.'
     else
@@ -20,14 +19,6 @@ class AffiliatesController < ApplicationController
   end
 
   private
-
-  def authorize_affiliate
-    authorize @affiliate || Affiliate
-  end
-
-  def set_affiliate
-    @affiliate = Affiliate.find(params[:id])
-  end
 
   def affiliate_params
     params.require(:affiliate).permit(:name, :stake)
--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,17 +1,16 @@
 class SuppliersController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_supplier
 
   def index
     @suppliers = policy_scope(Supplier)
   end
 
   def new
-    @supplier = Supplier.new
+    @supplier = authorize Supplier.new
   end
 
   def create
-    @supplier = Supplier.new(supplier_params)
+    @supplier = authorize Supplier.new(supplier_params)
     if @supplier.save
       redirect_to suppliers_path, notice: 'Supplier was successfully created.'
     else
@@ -23,9 +22,5 @@ class SuppliersController < ApplicationController
 
   def supplier_params
     params.require(:supplier).permit(:name, :supplier_number)
-  end
-
-  def authorize_supplier
-    authorize Supplier
   end
 end
