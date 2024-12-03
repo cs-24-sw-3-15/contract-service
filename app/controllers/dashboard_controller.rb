e6335f519631ebeefec6dd3779b382b59ab0363b
casstef <casper_steffensen@yahoo.co.uk>
2024-12-03 10:33:15 +0100

    Add piechart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,18 @@ class DashboardController < ApplicationController
   skip_after_action :verify_pundit_authorization
 
   def index
+
+    @contracts_pending = Contract.where(id: policy_scope(Contract).select(:id))
+                                 .where(status: :pending)
+                                 .group(:status)
+                                 .count
+
+    @contracts_expired = Contract.where(id: policy_scope(Contract).select(:id))
+                                 .where(state: [:active, :expired])
+                                 .group(:state)
+                                 .count
+
+    @contracts_data = @contracts_pending.merge(@contracts_expired) { |key, pending_count, expired_count| pending_count + expired_count }
+
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application"
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
