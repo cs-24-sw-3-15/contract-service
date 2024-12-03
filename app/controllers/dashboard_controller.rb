53c6c2367cba618bc6941e6410d2b574a23ae3b6
casstef <casper_steffensen@yahoo.co.uk>
2024-12-03 11:05:25 +0100

    Fix lint

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,18 +4,16 @@ class DashboardController < ApplicationController
   skip_after_action :verify_pundit_authorization
 
   def index
-
     @contracts_pending = Contract.where(id: policy_scope(Contract).select(:id))
                                  .where(status: :pending)
                                  .group(:status)
                                  .count
 
     @contracts_expired = Contract.where(id: policy_scope(Contract).select(:id))
-                                 .where(state: [:active, :expired])
+                                 .where(state: [ :active, :expired ])
                                  .group(:state)
                                  .count
 
     @contracts_data = @contracts_pending.merge(@contracts_expired) { |key, pending_count, expired_count| pending_count + expired_count }
-
   end
 end
