5efff4c624071b476f78777625c069bec47c7303
OskarSun <144021227+OskarSun@users.noreply.github.com>
2024-12-04 10:43:51 +0100

    Gorgeous for good looks, can push asap

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,15 @@ class DashboardController < ApplicationController
   skip_after_action :verify_pundit_authorization
 
   def index
+
     @contracts_status = policy_scope(Contract).group(:status).count
     @contracts_approved_state = policy_scope(Contract).approved.group(:state).count
+
+    today = Date.today
+    four_weeks_ahead = today + 4.weeks
+
+    @soon_active_contracts = policy_scope(Contract).where(start_date: today..four_weeks_ahead)
+    @soon_expiring_contracts = policy_scope(Contract).where(end_date: today..four_weeks_ahead)
+
   end
 end
